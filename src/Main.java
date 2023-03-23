079dc3210a47f5abc9f94ef856f50a43ca65af34
KarliEST <karlikullasepp@gmail.com>
2023-03-23 18:06:15 +0200

    Loodud objekt (TestDTO), mis täidetakse dokumendis oleva rea andmetega. Sorteerimine töötab.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        Integer sessionId = 80;
+
         ReadFromFile readFromFile = new ReadFromFile();
-        readFromFile.readFromFile();
-        System.out.println(readFromFile.readFromFile());
+
+        List<TestDTO> allData = new ArrayList<>();
+        allData = readFromFile.readFromFile();
+
+        // Sorteerimata väljad.
+        System.out.println("Prindib kõik väljad originaal kujul");
+        for (var line : allData
+        ) {
+            System.out.println(line);
+        }
+
+        // Sorteerimine timestamp järgi.
+        Collections.sort(allData, new Comparator<TestDTO>() {
+            @Override
+            public int compare(TestDTO t0, TestDTO t1) {
+                return t0.getTimestamp()- t1.getTimestamp();
+            }
+        });
+
+        // Prindi sorteeritud väljad.
+        System.out.println("Prindib kõik väljad timestamp-i järgi sorteeritult");
+        for (var line : allData
+        ) {
+            System.out.println(line);
+        }
+
+        // Prindi sessionId väljad.
+        System.out.println("Prindib väljad sessionId-ga: "+sessionId);
+        for (var line : allData
+        ) {
+            if (line.getSessionId().equals(sessionId)) {
+                System.out.println(line);
+            }
+        }
+
+//        System.out.println(readFromFile.readFromFile());
+
+//        var data = readFromFile.readFromFile();
+//        var newArray = new ArrayList<>();
+
+
+//        for (Object item : data
+//        ) {
+//            System.out.println(item.toString());
+//        }
+
 
     }
 }
--- src/ReadFromFile.java
+++ src/ReadFromFile.java
@@ -9,8 +9,7 @@ public class ReadFromFile {
 
     public List readFromFile() {
 
-//        List<List> array= new ArrayList<>(5);
-        var array = new ArrayList<>(6);
+        var array = new ArrayList<>();
 
         try {
             File myObj = new File("./resources/game_data_1.txt");
@@ -18,15 +17,24 @@ public class ReadFromFile {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 List<String> line = Arrays.asList(data.split("\\s*,\\s*"));
-                ArrayList sessionLine = new ArrayList<>();
+//                ArrayList sessionLine = new ArrayList<>();
                 if (line.size() == 6) {
-                    sessionLine.add(Integer.parseInt(line.get(0)));
-                    sessionLine.add(Integer.parseInt(line.get(1)));
-                    sessionLine.add(Integer.parseInt(line.get(2)));
-                    sessionLine.add(line.get(3).toUpperCase());
-                    sessionLine.add(line.get(4).toUpperCase());
-                    sessionLine.add(line.get(5).toUpperCase());
-                    array.add(sessionLine);
+                    TestDTO testDTO = new TestDTO(
+                            Integer.parseInt(line.get(0)),
+                            Integer.parseInt(line.get(1)),
+                            Integer.parseInt(line.get(2)),
+                            line.get(3).toUpperCase(),
+                            line.get(4).toUpperCase(),
+                            line.get(5).toUpperCase()
+                    );
+                    array.add(testDTO);
+//                    sessionLine.add(Integer.parseInt(line.get(0)));
+//                    sessionLine.add(Integer.parseInt(line.get(1)));
+//                    sessionLine.add(Integer.parseInt(line.get(2)));
+//                    sessionLine.add(line.get(3).toUpperCase());
+//                    sessionLine.add(line.get(4).toUpperCase());
+//                    sessionLine.add(line.get(5).toUpperCase());
+//                    array.add(sessionLine);
                 }
             }
             myReader.close();
