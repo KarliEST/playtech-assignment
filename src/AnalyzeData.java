f5fb9f19a0311a64fccede8bd35a776a97d4d574
KarliEST <karlikullasepp@gmail.com>
2023-03-24 11:28:57 +0200

    Lisatud testimised checkJoinedCardCount,getHandValue,checkCards,getHandArray

--- src/AnalyzeData.java
+++ src/AnalyzeData.java
@@ -1,9 +1,59 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class AnalyzeData {
 
-    public void analyze() {
-        ReadFromFile read = new ReadFromFile();
-        Deck deck = new Deck();
+    public Boolean checkJoinedCardCount(String dealersHand, String playersHand, String status) {
+        String[] dhand = getHandArray(dealersHand);
+        String[] phand = getHandArray(playersHand);
+        boolean result = false;
+        // TODO: kustuta testimisel kasutatud soutid
+        System.out.println("dealers hand: " + Arrays.toString(dhand));
+        System.out.println("players hand: " + Arrays.toString(phand));
+        if (Objects.equals(status, "P Joined") && dhand.length != 2 ||
+                Objects.equals(status, "P Joined") && phand.length != 2) {
+            result = true;
+            // TODO: kustuta testimise sout
+            System.out.println("Error in starting, card count is false at joining!!");
+        }
+        return result;
+    }
 
+    public Integer getHandValue(String hand) {
+        int value = 0;
+        Deck deck = new Deck();
+        // TODO: kustuta sout
+        System.out.println("Hand: " + hand);
+        for (String card : getHandArray(hand)
+        ) {
+            value += deck.getCardValue(card);
+        }
+        // TODO: kustuta sout
+        System.out.println("Hand value: " + value);
+        return value;
+    }
+
+    public Boolean checkCards(String hand) {
+        Deck deck = new Deck();
+        String[] validCards = deck.getCardDeck();
+        // TODO: kustuta sout
+        System.out.println("Hand: " + hand);
+        boolean result = false;
+        for (String card : getHandArray(hand)) {
+            if (Arrays.stream(validCards).noneMatch(card::equals)) {
+                result = true;
+            }
+        }
+        // TODO: kustuta testimise sout
+        if (result) {
+            System.out.println("Hand contains card error!!");
+        }
+        return result;
+    }
+
+    private String[] getHandArray(String hand) {
+        String[] handArr = hand.split("-");
+        return handArr;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
-        Integer sessionId = 80;
-
-        ReadFromFile readFromFile = new ReadFromFile();
-
-        List<TestDTO> allData = new ArrayList<>();
-        allData = readFromFile.readFromFile();
-
-        // Sorteerimata väljad.
-        System.out.println("Prindib kõik väljad originaal kujul");
-        for (var line : allData
-        ) {
-            System.out.println(line);
-        }
-
-        // Sorteerimine timestamp järgi.
-        allData.sort((t0, t1) -> t0.getTimestamp() - t1.getTimestamp());
-
-        // Prindi sorteeritud väljad.
-        System.out.println("Prindib kõik väljad timestamp-i järgi sorteeritult");
-        for (var line : allData
-        ) {
-            System.out.println(line);
-        }
-
-        // Prindi sessionId väljad.
-        System.out.println("Prindib väljad sessionId-ga: " + sessionId);
-        for (var line : allData
-        ) {
-            if (line.getSessionId().equals(sessionId)) {
-                System.out.println(line);
-            }
-        }
-
-//        System.out.println(readFromFile.readFromFile());
-
-//        var data = readFromFile.readFromFile();
-//        var newArray = new ArrayList<>();
-
-
-//        for (Object item : data
+//        Integer sessionId = 1;
+//
+//        ReadFromFile readFromFile = new ReadFromFile();
+//
+//        List<TestDTO> allData = new ArrayList<>();
+//        allData = readFromFile.readFromFile();
+//
+//        // Sorteerimata väljad.
+//        System.out.println("Prindib kõik väljad originaal kujul");
+//        for (var line : allData
 //        ) {
-//            System.out.println(item.toString());
+//            System.out.println(line);
+//        }
+//
+//        // Sorteerimine timestamp järgi.
+//        allData.sort((t0, t1) -> t0.getTimestamp() - t1.getTimestamp());
+//
+//        // Prindi sorteeritud väljad.
+//        System.out.println("Prindib kõik väljad timestamp-i järgi sorteeritult");
+//        for (var line : allData
+//        ) {
+//            System.out.println(line);
+//        }
+//
+//        // Prindi sessionId väljad.
+//        System.out.println("Prindib väljad sessionId-ga: " + sessionId);
+//        for (var line : allData
+//        ) {
+//            if (line.getSessionId().equals(sessionId)) {
+//                System.out.println(line);
+//            }
 //        }
 
+
+        AnalyzeData analyzeData = new AnalyzeData();
+        analyzeData.getHandValue("KH-?");
+        System.out.println(analyzeData.checkCards("KH-8F-?"));
+        analyzeData.checkJoinedCardCount("KH-?","KH-8H","P Joined");
 
     }
 }
