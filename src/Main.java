818107702e2382345819f60c18eed1447ab30b38
KarliEST <karlikullasepp@gmail.com>
2023-03-26 18:12:33 +0300

    pre-test commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         DataAnalyzer dataAnalyzer = new DataAnalyzer();
-        String inputFile = "game_data_2.txt";
-        String outputFile = "analyzer_output.txt";
+        String inputFile = "game_data.txt";
+        String outputFile = "analyzer_results.txt";
         dataAnalyzer.analyzeData(inputFile,outputFile);
 
     }
